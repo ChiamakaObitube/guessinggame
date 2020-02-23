cd46621861f3769bb6d35b0371068632262933ee
Chiamaka Obitube <obitubechiamaka@gmail.com>
2020-02-23 12:15:27 +0100

    initial commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 number_of_files=10
+number_of_guesses
 for (( ; ; ))
 do
     read -p "Guess the number of files in this directory: " user_number
     if [ $user_number -eq $number_of_files ]
     then
         echo "Your guess is correct"
+        echo "You guessed with these $number_of_chances"
         break
     else
         echo "your guess is wrong"
@@ -17,4 +19,5 @@ do
             echo "Please select a number higher than $user_number"
         fi
     fi
+    number_of_chances=$((number_of_chances+1))
     done
