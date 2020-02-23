882d74a7368a0c0071e9c75d81ef54ad61cbb967
Chiamaka Obitube <obitubechiamaka@gmail.com>
2020-02-23 18:48:27 +0100

    add changes and generate README.md

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-number_of_files=$RANDOM
+number_of_files=$(ls -l . | egrep -c '^-')
 number_of_guesses=1
-for (( ; ; ))
-do
+function guess {
+  while true;
+  do
     read -p "Guess the number of files in this directory: " user_number
-    if [ $user_number -eq $number_of_files ]
-    then
-        echo "Your guess is correct"
-        echo "You guessed right with these $number_of_guesses guesses"
-        break
-    else
-        echo "your guess is wrong"
-        if [ $user_number -ge $number_of_files ]
+         if [[ $user_number == *[^[:digit:]]* ]] 
         then
-            echo "please select a number lesser than $user_number"
-        else
-            echo "Please select a number higher than $user_number"
+        echo "Please enter a valid input";
         fi
-    fi
+        if [ $user_number -eq $number_of_files ]
+        then
+            echo "Your guess is correct"
+            echo "You guessed right with these $number_of_guesses guesses"
+            break
+        else
+            echo "your guess is wrong"
+                if [ $user_number -ge $number_of_files ]
+                    then
+                    echo "please select a number lesser than $user_number"
+              else
+                    echo "Please select a number higher than $user_number"
+                fi
+        fi
     number_of_guesses=$((number_of_guesses+1))
-    done
+  done
+}
+guess
